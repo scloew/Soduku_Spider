cbec78d6581d38b39c9dcc69fff23e32dc9756c9
scloew <stevecloew@gmail.com>
2020-03-02 08:32:37 -0600

    started pipeline
    started working with pipeling but getting an error in defer.py about number of positional arguments being given

--- Soduku_Spider/pipelines.py
+++ Soduku_Spider/pipelines.py
@@ -4,8 +4,37 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pyodbc
 
 class SodukuSpiderPipeline(object):
-    def process_item(self, item, spider):
+
+    def __init__(self):
+        self.conn = pyodbc.connect(
+            "Driver={SQL Server};"
+            "Server=LAPTOP-B3HSU0AK\LOEWSQL;"
+            "Database=SodukuPuzzles;"
+            "Trusted_Connection=yes"
+        )
+
+    def _insert(self, item):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'INSERT into Puzzles(difficulty, solution, mask)'
+            'values(?,?,?);',
+            (
+                item['difficulty'],
+                item['solution'],
+                item['mask'],
+                0
+            )
+        )
+        self.conn.commit()
+
+    def process_item(self, item):
+        # self._insert(item)
+        # print("'ello governor")
+        # with open('out2.txt', 'a') as f:
+        #     f.write(item.difficulty)
+        #     f.write(item.solution)
+        #     f.write(item.mask)
         return item
--- Soduku_Spider/spiders/puzzle_spider.py
+++ Soduku_Spider/spiders/puzzle_spider.py
@@ -1,23 +1,35 @@
 import scrapy
+from collections import namedtuple
+
+
+class Puzzle(scrapy.Item) :
+  difficulty = scrapy.Field()
+  solution = scrapy.Field()
+  mask = scrapy.Field()
 
 class PuzzleSpider(scrapy.Spider):
+  name = 'puzzles'
+  start_urls = [
+    f'https://nine.websudoku.com/?level={lvl}&set_id={puz_num}'
+    for lvl in range(1, 2) for puz_num in range(75, 77)
+  ]
 
-    name = 'puzzle_spider'
-    start_urls = [f'https://nine.websudoku.com/?level={lvl}&set_id={puz_num}' \
-                  for lvl in range(1, 5) for puz_num in range(75, 77)]
-    #TODO currently only points to one puzzle; need it to point to others
-
-    def parse(self, response):
-      difficulties = {'Easy', 'Medium', 'Hard', 'Evil'}
-      with open('out.txt', 'a') as f:
-        f.write('hello puzzle\n')
-        solution = response.css('[name="cheat"]').xpath('@value').extract()[0]
-        mask = response.css('[id="editmask"]').xpath('@value').extract()[0]
-        category = response.css('p font a[title="Copy link for this puzzle"]::text').extract()[0]
-        difficulty = difficulties.intersection(set(category.split(' ')))
-        f.write(f'{solution}\n')
-        f.write(f'{mask}\n')
-        f.write(f'{difficulty.pop()}\n\n')
-      yield None
-#﻿body table[id="puzzle_grid"] td
-#TODO this is the chrome dev  tools css.... but doesn't seem to work here
+  def parse(self, response):
+    difficulties = {'Easy', 'Medium', 'Hard', 'Evil', 'unknown'}
+    category = response.css('p font a[title="Copy link for this puzzle"]::text').extract()[0]
+    difficulty = difficulties.intersection(set(category.split(' ')))
+    solution = response.css('[name="cheat"]').xpath('@value').extract()[0]
+    mask = response.css('[id="editmask"]').xpath('@value').extract()[0]
+    puzzle = Puzzle()
+    puzzle['difficulty'] = 'easy'
+    puzzle['solution'] = solution
+    puzzle['mask'] = mask
+    with open('out.txt', 'a') as f:
+      category = response.css('p font a[title="Copy link for this puzzle"]::text').extract()[0]
+      difficulty = difficulties.intersection(set(category.split(' ')))
+      solution = response.css('[name="cheat"]').xpath('@value').extract()[0]
+      mask = response.css('[id="editmask"]').xpath('@value').extract()[0]
+      f.write(f'{difficulty.pop()}\n')
+      f.write(f'{solution}\n')
+      f.write(f'{mask}\n\n')
+    yield puzzle
